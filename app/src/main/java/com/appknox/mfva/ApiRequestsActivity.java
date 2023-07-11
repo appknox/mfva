f8a28883b9f592de0453d6f667a5e28aaf4870e7
dhilipsiva <dhilipsiva@pm.me>
2023-07-11 18:31:05 +0530

    upgrade to android-x

--- app/src/main/java/com/appknox/mfva/ApiRequestsActivity.java
+++ app/src/main/java/com/appknox/mfva/ApiRequestsActivity.java
@@ -1,10 +1,10 @@
 package com.appknox.mfva;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,7 +13,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.UUID;
-import java.util.Random;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
