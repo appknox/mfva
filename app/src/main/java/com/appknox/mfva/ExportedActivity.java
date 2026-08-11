251e890f7c37f3a6faabf760ad1b799eee1a34bf
Shubham Singh <suvamsingh55@gmail.com>
2026-08-11 14:33:09 +0530

    fix(autofix): Derived Crypto Keys in ExportedActivity.java

--- app/src/main/java/com/appknox/mfva/ExportedActivity.java
+++ app/src/main/java/com/appknox/mfva/ExportedActivity.java
@@ -23,7 +23,7 @@ public class ExportedActivity extends AppCompatActivity {
         Jedis jedis = new Jedis("localhost");
 
         try {
-            Cipher.getInstance("DES/ECB/ZeroBytePadding", "BC");
+            Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
         } catch (NoSuchAlgorithmException|NoSuchProviderException|NoSuchPaddingException e) {
             // pass
         }
