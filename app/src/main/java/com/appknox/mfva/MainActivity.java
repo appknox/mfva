1dbdf69ecf976c1545c807daedfb6131c3c8e4c7
Shubham Singh <suvamsingh55@gmail.com>
2026-08-25 12:24:19 +0530

    Appknox Autofix: fix 1 file(s)

--- app/src/main/java/com/appknox/mfva/MainActivity.java
+++ app/src/main/java/com/appknox/mfva/MainActivity.java
@@ -19,6 +19,7 @@ import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -30,6 +31,8 @@ import javax.crypto.spec.SecretKeySpec;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final SecureRandom secureRandom = new SecureRandom();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                         "os.name",
                         "os.version",
                 };
-                String key = keys[(int) (Math.random() * keys.length)];
+                String key = keys[secureRandom.nextInt(keys.length)];
                 editor.putString(key, System.getProperty(key));
                 editor.commit();
 
@@ -109,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                         "Every program has two purposes ― one for which it was written and another for which it wasn't.",
                         "Every program is a part of some other program, and rarely fits.",
                 };
-                String quote = quotes[(int) (Math.random() * quotes.length)];
+                String quote = quotes[secureRandom.nextInt(quotes.length)];
                 Log.d("YOLO", quote);
                 Snackbar.make(v, quote, Snackbar.LENGTH_SHORT).show();
             }
@@ -119,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
         buttonCalculateChecksum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String quote = "The beauty of me is that I’m very rich. - Donald Trump";
+                String quote = "The beauty of me is that I'm very rich. - Donald Trump";
                 try {
                     MessageDigest md = MessageDigest.getInstance("MD5");
                     byte[] hash = md.digest(quote.getBytes());
